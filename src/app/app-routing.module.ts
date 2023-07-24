66c4009047509b77850cbcb5337119c4b76896eb
otsembo <okumu.otsembo@gmail.com>
2023-07-24 14:46:25 +0300

    app add auth routing

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,12 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import {HomeComponent} from "./ui/home/home.component";
+import {AuthComponent} from "./ui/auth/auth.component";
 
-const routes: Routes = [];
+const routes: Routes = [
+  { component: HomeComponent, path: '' },
+  { component: AuthComponent, path: 'auth' }
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
