a0152f0e0b3615acb661ce39c7ed20af3bc6a07f
otsembo <okumu.otsembo@gmail.com>
2023-07-25 14:04:00 +0300

    Add transactions page

--- src/app/ui/components/transaction/transaction.component.ts
+++ src/app/ui/components/transaction/transaction.component.ts
@@ -9,6 +9,8 @@ import {Transaction} from "../../../data/models/transaction";
 export class TransactionComponent {
   @Input() transaction?: Transaction;
   isNegative?: boolean = this.transaction?.from == this.transaction?.partyA;
+  private headerColors: string[] = [ 'bg-red-500', 'bg-green-500', 'bg-yellow-500', 'bg-cyan-500', 'bg-teal-500', 'bg-gray-500', 'bg-blue-500' ];
+  headerColor: string = this.headerColors[Math.floor(Math.random() * this.headerColors.length)]
   constructor() {
     console.log("Transaction", this.isNegative)
   }
